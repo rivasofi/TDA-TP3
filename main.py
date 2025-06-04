a615a0904a28d75ae34554aeff8fd058113bef43
sofi <srivas@fi.uba.ar>
2025-06-03 23:58:36 -0300

    Subo main para realizar pruebas

---
+++ main.py
@@ -0,0 +1,28 @@
+from parser import *
+from grafo import Grafo
+from backtracking import clustering_optimizacion
+import sys
+import time
+
+def imprimir_asignaciones(clusters, diametro, duracion):
+    print("Asignación:")
+    for cluster, vertices in clusters.items():
+        print(f"{cluster}: {vertices}")
+    print("Maxima distancia dentro del cluster:", diametro)
+    print(f"Tiempo total de ejecución: {duracion:.2f} segundos")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Uso: python main.py <archivo_grafo> <k>")
+        sys.exit(1)
+
+    #ruta predefinida como archivos_catedra/archivo
+    #funciona con txt y csv
+    archivo = f"archivos_catedra/{sys.argv[1]}"
+    k = int(sys.argv[2])
+
+    grafo_trabajo = cargar_grafo(archivo)
+    inicio = time.time()
+    clusters, diametro = clustering_optimizacion(grafo_trabajo, k)
+    fin = time.time()
+    imprimir_asignaciones(clusters, diametro, fin - inicio)
